c3e3e3765b21536caa3cf484e8f11ef917accc4f
DocMTyler <tylerhougland@att.net>
2020-11-18 16:11:06 -0500

    Added many functions

---
+++ ElevenNote/ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2,ErrorMessage = "Please enter more than 2 characters for the title")]
+        [MaxLength(100, ErrorMessage = "Slow down there, Geez! Enter less than 100 characters for the title")]
+        public string Title { get; set; }
+
+        [MaxLength(8000)]
+        public string Content { get; set; }
+    }
+}
